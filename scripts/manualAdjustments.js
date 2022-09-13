b9c36cd802f74686fd5dece0170796c2249a57d5
Prekomandir <filip.kulas0101@gmail.com>
2022-09-14 00:00:18 +0200

    new file:   scripts/manualAdjustments.js

---
+++ scripts/manualAdjustments.js
@@ -0,0 +1,6 @@
+exports.manuallyAdded = [
+    //justification: tried converting from old CRSS version to CRSSv11, after the attackers minted to max supply of CRSS, lost CRSS in the process
+    { "address": "0x4D473bC97aDF3B6bd48F28a80a495964Bd67e13b", "crssOwed": 105 },
+    //justification: tried converting from old CRSS version to CRSSv11, after the attackers minted to max supply of CRSS, lost CRSS in the process
+    { "address": "0x7BE73921626702D045B69E47bA029572b5d0f768", "crssOwed": 948.518 }
+]
